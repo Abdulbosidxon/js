39f6400f9f99406a11a72b0f21d77aca8bc7f4bf
Abdulbosit7792 <abdulbosid7792@gmail.com>
2024-01-19 21:32:38 +0500

    js

---
+++ JavaScript.js
@@ -0,0 +1,7 @@
+const ism ="Abdulbosit";
+
+let yosh = 12;
+let qiziqish = "futbol dasturlash";
+let texnologiya ="texnologiyalarim Html Css bootstrap"; 
+
+console.log(`salom mening ismim ${ism} va men ${yosh} yoshdaman hozirda 32-maktabda uqiyman qiziqishlarim ${qiziqish} Foydalangan ${texnologiya} "`);
